cd9488082d5f907edcce712875606032a45efd1d
medricet <medricet@gmail.com>
2022-10-29 23:17:48 -0400

    added api

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 8000;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send({});
 });
 
-app.get('/user/:name', (req, res) => {
-  res.send({
-    name: req.params.name,
-    age: 18,
-  });
+app.get('/course/:course', async (req, res) => {
+  let course = req.params.course;
+  
+  if (course.match(/^[a-zA-Z0-9]+$/)) {
+    let api_response = await fetch("https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2225&course-code=" + course);
+    let json = await api_response.json();
+  
+    res.send(json);
+  } else {
+    res.send({ request_error: true })
+  }
 });
 
 app.listen(port, () => {
